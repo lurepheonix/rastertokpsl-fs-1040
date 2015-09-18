42bdc7ce8e56dec71776482f3b77f6610f76525a
The Master <djblasterx@gmail.com>
2015-09-18 08:03:23 +0300

    patches for gentoo

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ fi
 if [ ! -d /usr/share/cups/model/Kyocera ]; then
 sudo mkdir /usr/share/cups/model/Kyocera
 fi
-sudo cp Kyocera_FS-1040GDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1040GDI.ppd
-sudo cp rastertokpsl /usr/lib/cups/filter/rastertokpsl
-sudo cp rastertokpsl-fixed /usr/lib/cups/filter/rastertokpsl-fixed
-sudo chmod +x /usr/lib/cups/filter/rastertokpsl-fixed
-if [ -f /usr/lib/cups/filter/rastertokpsl ] && [ -f /usr/lib/cups/filter/rastertokpsl-fixed ] && [ -f /usr/share/cups/model/Kyocera/Kyocera_FS-1040GDI.ppd ]; then
+sudo cp -vf Kyocera_FS-1040GDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1040GDI.ppd
+sudo cp -vf rastertokpsl /usr/libexec/cups/filter/rastertokpsl
+sudo cp -vf rastertokpsl-fixed /usr/libexec/cups/filter/rastertokpsl-fixed
+sudo chmod +x /usr/libexec/cups/filter/rastertokpsl-fixed
+if [ -f /usr/libexec/cups/filter/rastertokpsl ] && [ -f /usr/libexec/cups/filter/rastertokpsl-fixed ] && [ -f /usr/share/cups/model/Kyocera/Kyocera_FS-1040GDI.ppd ]; then
 echo "Installation completed"
 else
 echo "Installation failed"
